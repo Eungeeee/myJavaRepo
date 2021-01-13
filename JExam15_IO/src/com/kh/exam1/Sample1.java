bb1aa81a4b3f3824a67383b0fb39fbf4c4db66c5
Eungeeee <eungeeee1002@gmail.com>
2021-01-13 09:12:57 +0900

    보충

--- JExam15_IO/src/com/kh/exam1/Sample1.java
+++ JExam15_IO/src/com/kh/exam1/Sample1.java
@@ -2,24 +2,110 @@ package com.kh.exam1;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Sample1 {
+	
+	private static File f;
+	
+	public static String[] getDirList(String path) {
+		/*
+		 *  지정한 디렉터리의 모든 파일 및 디렉터리 목록을 반환하는 메서드
+		 *  반환형은 문자열 배열
+		 *  단, 디렉터리만 반환한다.
+		 */
+		f = new File(path);
+		if(!f.exists()) {
+			System.out.println(path + " 에 해당하는 디렉터리 또는 파일이 존재하지 않습니다.");
+			return new String[] {""};
+		}
+		
+		File[] fList = f.listFiles();
+		String[] sList = new String[fList.length];
+		int idx = 0;
+		for(int i = 0; i < fList.length; i++) {
+			if(fList[i].isDirectory()) {
+				sList[idx] = fList[i].getName();
+				idx++;
+			}
+		}
+		String[] res = new String[idx];
+		System.arraycopy(sList, 0, res, 0, idx);
+		return res;
+	}
+	
+	public static String[] getFileList(String path) {
+		/*
+		 *  지정한 디렉터리의 모든 파일 및 디렉터리 목록을 반환하는 메서드
+		 *  반환형은 문자열 배열
+		 *  단, 파일만 반환한다.
+		 */
+		f = new File(path);
+		if(!f.exists()) {
+			System.out.println(path + " 에 해당하는 디렉터리 또는 파일이 존재하지 않습니다.");
+			return new String[] {""};
+		}
+		
+		File[] fList = f.listFiles();
+		String[] sList = new String[fList.length];
+		int idx = 0;
+		for(int i = 0; i < fList.length; i++) {
+			if(fList[i].isFile()) {
+				sList[idx] = fList[i].getName();
+				idx++;
+			}
+		}
+		String[] res = new String[idx];
+		System.arraycopy(sList, 0, res, 0, idx);
+		return res;
+	}
+	
+	public static String[] getList(String path) {
+		/*
+		 *  지정한 디렉터리의 모든 파일 및 디렉터리 목록을 반환하는 메서드
+		 *  반환형은 문자열 배열
+		 *  단, 숨김파일에 대해서는 반환하지 않게 만든다.
+		 */
+		f = new File(path);
+		if(!f.exists()) {
+			System.out.println(path + " 에 해당하는 디렉터리 또는 파일이 존재하지 않습니다.");
+			return new String[] {""};
+		}
+		
+		File[] fList = f.listFiles();
+		String[] sList = new String[fList.length];
+		int idx = 0;
+		for(int i = 0; i < fList.length; i++) {
+			if(!fList[i].isHidden()) {
+				sList[idx] = fList[i].getName();
+				idx++;
+			}
+		}
+		String[] res = new String[idx];
+		System.arraycopy(sList, 0, res, 0, idx);
+		return res;
+	}
 
 	public static void main(String[] args) {
+		
+		System.out.println(Arrays.toString(getList("/Users/moongjee/")) + "\n");
+		System.out.println(Arrays.toString(getDirList("/Users/moongjee/")) + "\n");
+		System.out.println(Arrays.toString(getFileList("/Users/moongjee/")) + "\n");
+		
 		File f = new File("/Users/moongjee/Desktop/Java/Sample.txt");
 		try {
 			f.createNewFile();
 		} catch (IOException e) {
 			System.out.println("IOException 에러 발생");
 		}
-		System.out.println(f.getName() + " 읽기 가능? : " + f.canRead());
-		System.out.println(f.getName() + " 쓰기 가능? : " + f.canWrite());
-		System.out.println(f.getName() + " 실행 가능? : " + f.canExecute());
-		System.out.println(f.getName() + " 디렉터리? : " + f.isDirectory());
-		System.out.println(f.getName() + " 파일? : " + f.isFile());
-		System.out.println(f.getName() + " 숨김? : " + f.isHidden());
-		System.out.println(f.getName() + " 크기(바이트)? : " + f.length());
-		System.out.println(f.getName() + " 마지막 수정날짜 : " + f.lastModified());
+		System.out.println(f.getName() + " 읽기 가능? : " + f.canRead() + "\n");
+		System.out.println(f.getName() + " 쓰기 가능? : " + f.canWrite() + "\n");
+		System.out.println(f.getName() + " 실행 가능? : " + f.canExecute() + "\n");
+		System.out.println(f.getName() + " 디렉터리? : " + f.isDirectory() + "\n");
+		System.out.println(f.getName() + " 파일? : " + f.isFile() + "\n");
+		System.out.println(f.getName() + " 숨김? : " + f.isHidden() + "\n");
+		System.out.println(f.getName() + " 크기(바이트)? : " + f.length() + "\n");
+		System.out.println(f.getName() + " 마지막 수정날짜 : " + f.lastModified() + "\n");
 		
 		f = new File("/Users/moongjee/Desktop/Java/");
 		String[] fileList = f.list();
@@ -31,7 +117,7 @@ public class Sample1 {
 		File[] fList = f.listFiles();
 		System.out.println(f.list());
 		for(int i = 0; i < fList.length; i++) {
-			System.out.println(fList[i] + "\t" + fList[i].isHidden() + "\t" + fList[i].length());;
+			System.out.println(fList[i] + "\t" + fList[i].isHidden() + "\t" + fList[i].length());
 		}
 		
 		f = new File("/Users/moongjee/Desktop/Java/newFolder");
--- JExam15_IO/src/com/kh/exam3/Sample3.java
+++ JExam15_IO/src/com/kh/exam3/Sample3.java
@@ -7,13 +7,102 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Sample3 {
+	
+	public static int wordCount(String filename, String word) {
+		/*
+		 * 지정한 파일의 문자열에서 word에 해당하는 단어가 몇개 있는지 반환하는 메서드
+		 */
+		try {
+			File f = new File(filename);
+			FileReader fr = new FileReader(f);
+			int read;
+			char[] cArr = new char[1024];
+			String s = "";
+			while((read = fr.read(cArr)) != -1) {
+				s += new String(cArr, 0, read);
+			}
+			int stIdx = 0;
+			int foundCount = 0;
+			while(true) {
+				stIdx = s.indexOf(word, stIdx);
+				if(stIdx == -1) {
+					break;
+				}
+				foundCount += 1;
+				stIdx = stIdx + 1;
+			}
+			fr.close();
+			return foundCount;
+		} catch(FileNotFoundException e) {
+			System.out.println("해당 파일이 존재하지 않습니다.");
+		} catch(IOException e) {
+			System.out.println("파일 입출력에 문제가 발생 했습니다.");
+		}
+		return 0;
+	}
+	
+	
+	public static void saveDirFileList(String dir) {
+		/*
+		 * 지정한 디렉터리 안의 폴더와 파일의 목록을 파일로 만들어 출력
+		 * 숨김 파일/폴더, 일반 파일/폴더를 구분하여 저장한다.
+		 */
+		File f = new File(dir);
+		
+		if(!f.exists()) {
+			System.out.println(dir + " 에 해당하는 디렉터리 또는 파일이 존재하지 않습니다.");
+		}
+		
+		File[] fList = f.listFiles();
+		// nFile : 일반 파일 문자열 | nFolder : 일반 디렉터리 | dFile : 숨김 파일 | dFolder : 숨김 디렉터리
+		String nFile = "", nFolder = "", dFile = "", dFolder = "";
+		for(int i = 0; i < fList.length; i++) {
+			if(fList[i].isFile()) {
+				if(fList[i].isHidden()) {
+				dFile += fList[i].getName();
+				} else {
+					nFile += fList[i].getName();
+				}
+			} else if (fList[i].isDirectory()){
+				if(fList[i].isHidden()) {
+					dFolder += fList[i].getName() + ", ";
+				} else {
+					nFolder += fList[i].getName() + ", ";
+				}
+			}
+		}
+		try {
+		FileWriter fw = new FileWriter(dir + "/list.txt");
+		fw.write(dir + "의 파일 목록\n");
+		fw.write("\t숨김 파일\n");
+		fw.write("\t\t" + dFile + "\n");
+		fw.write("\t숨김 폴더\n");
+		fw.write("\t\t" + dFolder + "\n");
+		fw.write("\t폴더\n");
+		fw.write("\t\t" + nFolder + "\n");
+		fw.write("\t파일\n");
+		fw.write("\t\t" + nFile + "\n");
+		fw.close();
+		System.out.println(dir + "/list.txt 위치에 파일 쓰기가 완료되었습니다.");
+		} catch (FileNotFoundException e) {
+			System.out.println("해당 파일 및 디렉터리가 존재하지 않습니다.");
+		} catch (IOException e) {
+			System.out.println("파일 입출력에 오류가 발생했습니다.");
+		}
+	}
 
+	
 	public static void main(String[] args) {
 		/**
 		 * 	FileReader / FileWriter
 		 * 		- 파일로 부터 문자 단위의 데이터 읽기 / 쓰기
 		 * 		- 주로 텍스트 파일에 대한 처리를 담당
 		 */
+		String filename = "/Users/moongjee/Library/Containers/jp.naver.line.mac/Data/Library/Containers/jp.naver.line/Data/LINE.ini";
+		System.out.println(wordCount(filename,"window"));
+		saveDirFileList("/Users/moongjee");
+		
+		
 		// 파일 읽기
 		try {
 			File f = new File("/Users/moongjee/Desktop/Java/Sample.txt");
